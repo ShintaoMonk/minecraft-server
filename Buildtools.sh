d4486a72aaa1f6266325b87bf0df4df261a5a994
= <ShintaoMonk@gmail.com>
2018-05-13 17:39:41 -0400

    Started File

--- Buildtools.sh
+++ Buildtools.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-#I think that's how you do it?
-echo bye
+link="https://hub.spigotmc.org/jenkins/job/BuildTools/lastStableBuild/artifact/target/BuildTools.jar"
+dir=$(pwd)
+
+if [ -e $dir'/BuildTools' ]
+then
+	echo "Found Dir"
+else
+	echo "Did not find directory, creating"
+	mkdir $dir'/BuildTools'
+fi
