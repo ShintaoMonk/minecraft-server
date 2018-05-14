9fad09afd7d94ec78248f535db28de2c38288910
= <ShintaoMonk@gmail.com>
2018-05-14 10:50:08 -0400

    Rewrote to consistant version

--- Settings.sh
+++ Settings.sh
@@ -2,93 +2,55 @@
 dir=$(pwd)
 # Get the file, as per naming convention (update for any changes on the file name)
 file=$dir'/settings'
+tmp=$dir'/.settings.tmp'
 
-tmpname=$dir"/.settings.tmp"
-
-#echo "setting directory = "$file
 if [[ $1 == "" ]]
 then
-	if [ -e $file ]
+	foo=foo
+else
+	#Get Action
+	action="change"
+	if [[ $2 == "" ]]
 	then
-		#echo "File Exists"
-		false=false
+		action="find"
 	else
-		#echo "Setting File does not exist"
-		#Create Settings
-		echo "#in format of {setting}={variable}" > settings
+		echo "" > $tmp
 	fi
-	
-	req=$1
-	val=$2
-	action="write"
-	if [[ $val == "" ]]
-	then
-		action="read"
-	else
-		echo "" > $tmpname
-	fi
-	
-	#Actual Scripts
-	
-	exists="false"
-	
 	cat $file | while read line
 	do
-		equal="false"
-		lnnme=${line%%=*}
-		lnval=${line#*=}
-		invis="false"
-	
-		if [[ $line == '#'* ]]
+		if [[ $line != "" && $line != "#"* ]]
 		then
-			equal='false'
-			invis="true"
-		else
-			if [[ $line == "" ]]
+			lnnme=${line%%=*}
+			lnvar=${line#*=}
+			if [[ $lnnme == $1 ]]
 			then
-				equal='false'
-				invis="true"
-			else
-				if [[ $lnnme == $req ]]
+				if [[ $action == "find" ]]
 				then
-					equal='true'
-				fi
-			fi
-		fi
-	
-		if [[ $action == "write" ]]
-		then
-			if [[ $invis == "true" ]]
-			then
-				echo $line >> $tmpname
-			else
-				if [[ $equal == "false" ]]
-				then
-					echo $lnnme"="$lnnme >> $tmpname
+					echo $lnvar
 				else
-					echo $lnnme"="$val >> $tmpname
-					exists="true"
+					echo $1"="$2 >> $tmp
+				fi
+			else
+				if [[ $action == "change" ]]
+				then
+					echo $line >> $tmp
 				fi
 			fi
 		else
-			if [[ $lnnme == $req ]]
+			if [[ $action == "change" ]]
 			then
-				echo $lnval
+				echo $line >> $tmp
 			fi
 		fi
 	done
-	
-	if [[ $action == "write" ]]
-	then
-		if [[ $exists == "false" ]]
-		then
-			echo $req"="$val >> $tmpname
-		fi
-		mv -u $dir'/setting.tmp' $tmpname
-	fi
-	
-	if [ -e $tmpname ]
-	then
-		rm $tmpname
-	fi
+fi
+
+if [[ $action == "change" ]]
+then
+	mv -u $tmp $file
+fi
+
+if [ -e $tmp ]
+then
+	rm $tmp
 fi
